5222a01a218e0afa542e3459581d839e13d65248
Jackson Pich <79122699+JayDiamond978@users.noreply.github.com>
2022-05-26 21:56:13 -0400

    Add files via upload

--- To Do List/app.js
+++ To Do List/app.js
@@ -1,3 +1,35 @@
+function loadPreservedData() {
+    const toDoList = document.querySelector("section > header");
+    const previousTasks = localStorage.getItem("saveCount");
+    //const previousTasksList = [];
+    for (let i = 0; i < parseInt(previousTasks); i++ ) {
+        //previousTasksList.push(JSON.parse(localStorage.getItem(`${i}`)));
+        let previousTask = JSON.parse(localStorage.getItem(`${i}`));
+
+        //Section
+        let toDoSection = document.createElement("section");
+        if (previousTask.Completed === true) {
+            toDoSection.style.textDecorationLine = "line-through";
+        }
+
+        //Checkbox
+        let toDoCheckBox = document.createElement("input");
+        toDoCheckBox.type = "checkbox";
+        toDoCheckBox.id = previousTask.ID;
+        if (previousTask.Completed === true) {
+            toDoCheckBox.checked = true;
+        }
+
+        //Label
+        let toDoLabel = document.createElement("label");
+        toDoLabel.textContent = previousTask.ID;
+        toDoLabel.setAttribute("for", previousTask.ID);
+
+        toDoSection.append(toDoCheckBox, toDoLabel);
+        toDoList.parentElement.append(toDoSection);
+    }
+}
+
 function addNewTask() {
     const newTask = document.querySelector("input[type=text]");
     const submitTaskButton = document.querySelector("button[type=submit]");
@@ -25,12 +57,12 @@ function addNewTask() {
         } else {
             //Can't use const or let in the for of loop, "Uncaught ReferenceError: task is not defined"
             for (task of allCurrentTasksButtons) {
-                console.log(newTask.value, task.id)
                 } if(newTask.value != task.id) {
                     newToDoSection.append(newToDoCheckBox, newToDoLabel);
                     toDoList.parentElement.append(newToDoSection);
                 }
             }
+        newTask.value = "";
     })
 }
 
@@ -53,7 +85,6 @@ function removeCompletedTasks() {
     const toDoList = document.querySelector(".toDoList");
     toDoList.addEventListener("dblclick", function(e){
         const allTaskCheckBoxes = document.querySelectorAll(".toDoList > section > input");
-        console.log(e.target);
         for (let taskCheckBox of allTaskCheckBoxes) {
             if (e.target.parentElement.style.textDecorationLine === "line-through"){
                 e.target.parentElement.remove();
@@ -76,12 +107,35 @@ function removeAllCompletedTasks() {
     })
 }
 
+function preserveData() {
+    window.addEventListener("beforeunload", function() {
+        const dataToSaveList = [];
+        const allTaskLines = document.querySelectorAll(".toDoList > section");
+        const allTaskBoxes = document.querySelectorAll(".toDoList > section > input");
+    
+        for (let i = 0; i < allTaskLines.length; i++) {
+            if (allTaskLines[i].style.textDecorationLine === "line-through") {
+                dataToSaveList.push({"ID" : `${allTaskBoxes[i].id}`, "Completed" : true});
+            } else {
+                dataToSaveList.push({"ID" : `${allTaskBoxes[i].id}`, "Completed" : false});
+            }
+        }
+    
+        for (let i = 0; i < dataToSaveList.length; i++) {
+            localStorage.setItem(`${i}`, JSON.stringify(dataToSaveList[i]));
+        }
+    
+        localStorage.setItem("saveCount", `${dataToSaveList.length}`);
+    })
+}
 
 function main() {
+    loadPreservedData();
     addNewTask();
     completeTask();
     removeCompletedTasks();
     removeAllCompletedTasks();
+    preserveData();
 }
 
 main();
